8f4e8b71cf330e79d272b96783f766760e37c3ff
jclegg03 <jjc2003@comcast.net>
2021-10-25 12:27:07 -0600

    added the int array and string array testing methods. used int array testing

--- src/array/controller/Controller.java
+++ src/array/controller/Controller.java
@@ -13,6 +13,28 @@ public class Controller
 	
 	public void start()
 	{
-		view.displayMessage("Hello there.");
+		stuffWithIntArray();
+		stuffWithStringArray();
+	}
+	
+	private void stuffWithIntArray()
+	{
+		int[] numbers = new int[5];
+		
+		numbers[4] = 12345;
+		numbers[0] = 99999;
+		numbers[2] = 1;
+		numbers[3] = -4321;
+		numbers[1] = 22;
+		
+		for(int index = 0; index < numbers.length; index++)
+		{
+			view.displayMessage("The index of " + index + " in the array contains the number: " + numbers[index]);
+		}
+	}
+	
+	private void stuffWithStringArray()
+	{
+		
 	}
 }
