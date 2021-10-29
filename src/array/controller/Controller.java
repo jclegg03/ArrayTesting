82c09df8ad73ccb87573cae34235aca0433150e0
jclegg03 <jjc2003@comcast.net>
2021-10-29 13:06:20 -0600

    added the sigma method

--- src/array/controller/Controller.java
+++ src/array/controller/Controller.java
@@ -194,4 +194,15 @@ public class Controller
 		}
 		return -1;
 	}
+	
+	private double sigma(double[] nums)
+	{
+		double sum = 0;
+		for(double num : nums)
+		{
+			sum += num;
+		}
+		return sum;
+	}
+	
 }
